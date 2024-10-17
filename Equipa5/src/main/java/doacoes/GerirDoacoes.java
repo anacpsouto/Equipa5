f3b1e0631c92a610c30a10bcf3cd0e131703328e
Ana Carolina P. Souto <anacarolinapsouto@gmail.com>
2024-10-17 08:47:06 +0100

    Atualização da classe Utilizador com a inclusão dos atributos: nome, email e password e geração de getters e setters. Criação da classe GerirDoacoes, da lista de utilizadores e do método adicionarUtilizador.

---
+++ Equipa5/src/main/java/doacoes/GerirDoacoes.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package doacoes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ */
+public class GerirDoacoes {
+	private List <Utilizador> listaUtilizadores;
+	
+	
+	public GerirDoacoes() {
+		listaUtilizadores = new ArrayList<>();
+	}
+	
+	
+	
+	public void adicionaUtilizador(Utilizador novoUtilizador) {
+		listaUtilizadores.add(novoUtilizador);
+	}
+	
+	
+	
+	
+	
+}
--- Equipa5/src/main/java/doacoes/Utilizador.java
+++ Equipa5/src/main/java/doacoes/Utilizador.java
@@ -9,4 +9,52 @@ package doacoes;
  */
 public class Utilizador {
 
+	private String nome; 
+	private String email;
+	private String password;
+	
+	
+	public Utilizador() {
+		
+	}
+	
+	/**
+	 * @param nome
+	 * @param email
+	 * @param password
+	 */
+	public Utilizador(String nome, String email, String password) {
+		this.nome = nome;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
 }

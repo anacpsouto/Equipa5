f8a7c6f2ca1e94698a8e0d9776f76d63622d0583
Ana Carolina P. Souto <anacarolinapsouto@gmail.com>
2024-10-20 00:21:38 +0100

    Teste de commit: tentando resolver status Unverifired nos meus commits

--- Equipa5/src/main/java/doacoes/Teste.java
+++ Equipa5/src/main/java/doacoes/Teste.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class Teste {
 	
 	public static void main(String[] args) {
-		GerirDoacoes gd = new GerirDoacoes();
+		//GerirDoacoes gd = new GerirDoacoes();
 		
 		Scanner sc = new Scanner(System.in);
 		

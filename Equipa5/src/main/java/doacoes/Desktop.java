cb56e8df4131701442e2db6d02f14adac93e4dca
User <User@DESKTOP-5LP1QR8.mshome.net>
2024-10-21 23:13:49 +0100

    classes dos equipamentos

---
+++ Equipa5/src/main/java/doacoes/Desktop.java
@@ -0,0 +1,57 @@
+package doacoes;
+
+public class Desktop {
+	private String placa;
+	private String processador;
+	private String cpu;
+	private int ano;
+	private int memoria;
+	
+	public Desktop( String placa, String processador, String cpu, int ano, int memoria) {
+		this.placa = placa;
+		this.processador = processador;
+		this.cpu = cpu;
+		this.ano = ano;
+		this.memoria = memoria;
+		}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getProcessador() {
+		return processador;
+	}
+
+	public void setProcessador(String processador) {
+		this.processador = processador;
+	}
+
+	public String getCpu() {
+		return cpu;
+	}
+
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public int getMemoria() {
+		return memoria;
+	}
+
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+}
---
+++ Equipa5/src/main/java/doacoes/Monitor.java
@@ -0,0 +1,59 @@
+package doacoes;
+
+public class Monitor extends Equipamento {
+	private int dimensao;
+	private int hertz;
+	private int temporesposta;
+	private int ano;
+	private String marca;
+	
+	public Monitor(int dimensao, int hertz,int ano, int temporesposta, String marca) {
+		this.dimensao = dimensao;
+		this.hertz = hertz;
+		this.ano = ano;
+		this.temporesposta = temporesposta;
+		this.marca = marca;
+	}
+
+	public int getDimensao() {
+		return dimensao;
+	}
+	
+	public void setDimensao(int dimensao) {
+		this.dimensao = dimensao;
+	}
+
+	public int getHertz() {
+		return hertz;
+	}
+
+	public void setHertz(int hertz) {
+		this.hertz = hertz;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public int getTemporesposta() {
+		return temporesposta;
+	}
+
+	public void setTemporesposta(int temporesposta) {
+		this.temporesposta = temporesposta;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+
+}

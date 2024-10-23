c4eaefae2e20869f829d9d41b8c47f4d41be677e
User <User@DESKTOP-5LP1QR8.mshome.net>
2024-10-23 20:23:20 +0100

    classes todas

--- Equipa5/src/main/java/doacoes/Acessorio.java
+++ Equipa5/src/main/java/doacoes/Acessorio.java
@@ -2,15 +2,31 @@ package doacoes;
 
 public class Acessorio extends Equipamento {
 	private String tipoAcessorio;
-	public Acessorio(String tipoAcessorio,String modelo,Tipo tipo, String estado, int ano) {
-		super(ano, estado, tipo, modelo);
+	private String informacao;
+	public Acessorio(String nome, String ano, String modelo, String estadoConservacao, String tipoAcessorio, String informacao) {
+		super(nome, ano, modelo, estadoConservacao);
 		this.tipoAcessorio = tipoAcessorio;
+		this.informacao = informacao;
+		
 	}
-	public String getTipoAcessorio() {
+	public String getTipo() {
 		return tipoAcessorio;
 	}
-	public void setTipoAcessorio(String tipoAcessorio) {
+	public void setTipo(String tipoAcessorio) {
 		this.tipoAcessorio = tipoAcessorio;
 	}
+	public String getInformacao() {
+		return informacao;
+	}
+	public void setInformacao(String informacao) {
+		this.informacao = informacao;
+	}
+	
+	@Override
+	public String toString() {
+		return ", Tipo de Acessório: " + tipoAcessorio + ", Informação : " + informacao;
+	}
+
+	
 	
 }
